209d843acb62307b211c8d5e9d63e5dea8229294
Aliesha Ellis <aliesha.christian@gmail.com>
2020-12-12 12:14:55 +1000

    Move fixture into its own file

---
+++ tests/fixtures.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+
+@pytest.fixture
+def temp_path_with_files(tmp_path):
+    # Windows uses case insensitive files by default - we need to enable case sensitive files
+    if os.name == 'nt':
+        os.system(f'cmd /c "fsutil.exe file setCaseSensitiveInfo {str(tmp_path)} enable"')
+
+    base_dir = tmp_path
+
+    # add a folder containing files with same case-insensitive names for testing
+    content_dir = base_dir / 'content_dir'
+    content_dir.mkdir()
+    names = ("afile.txt Afile.txt morefile.txt aFile.txt singlefile.txt adir ADIR "
+             "afile_1.txt AFile.txt blah.xls adoc.doc backup.BAK").split(' ')
+    for name in names:
+        filename = content_dir / name
+        filename.write_text("Text file contents")
+
+    return base_dir, content_dir
--- tests/test_rename.py
+++ tests/test_rename.py
@@ -1,28 +1,5 @@
-import os
-import pathlib
-
-import pytest
-
 from context import rename
-
-
-@pytest.fixture
-def temp_path_with_files(tmp_path):
-    # Windows uses case insensitive files by default - we need to enable case sensitive files
-    if os.name == 'nt':
-        os.system(f'cmd /c "fsutil.exe file setCaseSensitiveInfo {str(tmp_path)} enable"')
-
-    base_dir = tmp_path
-
-    # add a folder containing files with same case-insensitive names for testing
-    content_dir = base_dir / 'content_dir'
-    content_dir.mkdir()
-    names = "afile.txt Afile.txt morefile.txt aFile.txt singlefile.txt adir ADIR afile_1.txt AFile.txt".split(' ')
-    for name in names:
-        filename = content_dir / name
-        filename.write_text("Text file contents")
-
-    return base_dir, content_dir
+from fixtures import temp_path_with_files
 
 
 def test_rename_duplicates_in_directory_empty_directory_without_logfile(tmp_path):
