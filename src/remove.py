6d00e0236eaa7be7ebac82f8d551ced8d5e119e4
Aliesha Ellis <aliesha.christian@gmail.com>
2020-12-13 14:30:47 +1000

    Fix remove_empty_dir to check directory contents and added functionality to apply recursively

--- src/remove.py
+++ src/remove.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import pathlib
 import re
 
@@ -6,17 +7,61 @@ from .utilities import directory_is_empty
 
 
 log = logging.getLogger(__name__)
-       
+        
 
 def remove_empty_dir(
     dirpath: pathlib.Path,
     log_only: bool = False
-    ) -> bool:
+    ) -> None:
     """Remove the provided directory if empty"""
-    if directory_is_empty(dirpath):
+
+    directories_to_check = [dirpath]
+    if not directory_is_empty(dirpath):
+        directories_to_check = (path for path in dirpath.iterdir() if path.is_dir())
+
+    for directory in directories_to_check:
+        if directory_is_empty(directory):
+            if log_only:
+                log.info("Identified %s as an empty directory to remove", directory)
+            else:
+                directory.rmdir()
+                log.info("Removed %s as it was empty", directory)
+
+
+def remove_empty_dir_in_tree(
+    dirpath: pathlib.Path,
+    log_only: bool = False
+    ) -> None:
+    """Remove empty directories within the given directory tree"""
+    # topdown false starts walking from the bottom of the tree instead of the top
+    for root, _, _ in os.walk(dirpath, topdown=False):
+        remove_empty_dir(pathlib.Path(root), log_only=log_only)
+
+
+def remove_filepattern_in_dir(
+    dirpath: pathlib.Path,
+    pattern: str,
+    log_only: bool = False,
+    ) -> None:
+    """Remove files within the given directory if they match the given pattern"""
+
+    regex = re.compile(pattern, re.IGNORECASE)
+    
+    matches = [path for path in dirpath.iterdir() for m in [regex.search(path.name)] if m]
+    for path in matches:
         if log_only:
-            log.info("Identified %s as an empty directory to remove", dirpath)
+            log.info("Identified %s for removal as it matches %s", path, pattern)
         else:
-            dirpath.rmdir()
-            log.info("Removed %s as it was empty", dirpath)
-            
+            path.unlink()
+            log.info("Removed %s as it matched %s", path, pattern)
+
+
+def remove_filepattern_in_tree(
+    dirpath: pathlib.Path,
+    pattern: str,
+    log_only: bool = False,
+    ) -> None:
+    """Remove files within the given directory tree if they match the given pattern"""
+    # topdown false starts walking from the bottom of the tree instead of the top
+    for root, _, _ in os.walk(dirpath, topdown=False):
+        remove_filepattern_in_dir(pathlib.Path(root), pattern, log_only=log_only)
--- tests/test_remove.py
+++ tests/test_remove.py
@@ -2,14 +2,140 @@ from context import remove, utilities
 from fixtures import temp_path_with_files, info_caplog
 
 
-def test_remove_empty_dir(tmp_path, info_caplog):
-    assert tmp_path.exists()
-    remove.remove_empty_dir(tmp_path)
-    assert tmp_path.exists() is False
-    assert str(tmp_path) in info_caplog.text
+def test_remove_empty_dir(temp_path_with_files, info_caplog):
+    base_dir, content_dir = temp_path_with_files
 
-def test_remove_empty_dir_log_only(tmp_path, info_caplog):
-    assert tmp_path.exists()
-    remove.remove_empty_dir(tmp_path, log_only=True)
-    assert tmp_path.exists()
-    assert str(tmp_path) in info_caplog.text
+    empty_dirs = [
+        base_dir / 'empty_dir',
+        content_dir / 'ADIR'
+        ]
+
+    for edir in empty_dirs:
+        assert edir.exists()
+    
+    remove.remove_empty_dir(base_dir)
+    
+    assert empty_dirs[0].exists() is False
+    assert empty_dirs[1].exists()
+
+    assert str(empty_dirs[0]) in info_caplog.text
+    assert str(empty_dirs[1]) not in info_caplog.text
+
+
+def test_remove_empty_dir_log_only(temp_path_with_files, info_caplog):
+    base_dir, content_dir = temp_path_with_files
+
+    empty_dirs = [
+        base_dir / 'empty_dir',
+        content_dir / 'ADIR'
+        ]
+
+    for edir in empty_dirs:
+        assert edir.exists()
+    
+    remove.remove_empty_dir(base_dir, log_only=True)
+    
+    for edir in empty_dirs:
+        assert edir.exists()
+
+    assert str(empty_dirs[0]) in info_caplog.text
+    assert str(empty_dirs[1]) not in info_caplog.text
+
+
+def test_remove_empty_dir_in_tree(temp_path_with_files, info_caplog):
+    base_dir, content_dir = temp_path_with_files
+    assert base_dir.exists()
+
+    empty_dirs = [
+        base_dir / 'empty_dir',
+        content_dir / 'ADIR'
+        ]
+
+    remove.remove_empty_dir_in_tree(base_dir)
+
+    for edir in empty_dirs:
+        assert edir.exists() is False
+        assert str(edir) in info_caplog.text
+
+
+def test_remove_empty_dir_in_tree_log_only(temp_path_with_files, info_caplog):
+    base_dir, content_dir = temp_path_with_files
+    assert base_dir.exists()
+
+    empty_dirs = [
+        base_dir / 'empty_dir',
+        content_dir / 'ADIR'
+        ]
+    remove.remove_empty_dir_in_tree(base_dir, log_only=True)
+    
+    for edir in empty_dirs:
+        assert edir.exists()
+        assert str(edir) in info_caplog.text
+
+
+def test_remove_files_matching_pattern_in_dir(temp_path_with_files, info_caplog):
+    _, content_dir = temp_path_with_files
+
+    pattern = ".xls"
+
+    original_contents = list(str(c) for c in content_dir.iterdir())
+    assert len([fn for fn in original_contents if pattern in fn]) > 0
+
+    remove.remove_filepattern_in_dir(content_dir, pattern)
+
+    current_contents = list(str(c) for c in content_dir.iterdir())
+    assert len([fn for fn in current_contents if pattern in fn]) == 0
+
+
+def test_remove_files_matching_pattern_in_dir_log_only(temp_path_with_files, info_caplog):
+    _, content_dir = temp_path_with_files
+
+    pattern = ".xls"
+
+    original_contents = list(str(c) for c in content_dir.iterdir())
+    original_matches = [fn for fn in original_contents if pattern in fn]
+    assert len(original_matches) > 0
+
+    remove.remove_filepattern_in_dir(content_dir, pattern, log_only=True)
+
+    current_contents = list(str(c) for c in content_dir.iterdir())
+    current_matches = [fn for fn in current_contents if pattern in fn]
+    assert current_matches == original_matches
+
+
+def test_remove_files_matching_pattern_in_tree(temp_path_with_files, info_caplog):
+    _, content_dir = temp_path_with_files
+
+    pattern = ".xls"
+
+    matches = [
+        content_dir / 'adir' / "excel.xls",
+        content_dir / 'blah.xls'
+    ]
+
+    for m in matches:
+        assert m.exists()
+
+    remove.remove_filepattern_in_tree(content_dir, pattern)
+
+    for m in matches:
+        assert m.exists() is False
+
+
+def test_remove_files_matching_pattern_in_tree_log_only(temp_path_with_files, info_caplog):
+    _, content_dir = temp_path_with_files
+
+    pattern = ".xls"
+
+    matches = [
+        content_dir / 'adir' / "excel.xls",
+        content_dir / 'blah.xls'
+    ]
+
+    for m in matches:
+        assert m.exists()
+
+    remove.remove_filepattern_in_tree(content_dir, pattern, log_only=True)
+
+    for m in matches:
+        assert m.exists()
